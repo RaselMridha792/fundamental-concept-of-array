665d8e5ba6db47216e2401ff12958e976bf383ce
rasel mridha <raselmridha792@gmail.com>
2024-09-03 02:34:24 +0600

    push pop shift unshift of a array

---
+++ array.js
@@ -0,0 +1,8 @@
+const numbers = [12, 33, 44, 4];
+// console.log(numbers[2]);
+// console.log(numbers.length) for known a array length
+// numbers.push(32); for push a number on a array
+// numbers.pop() for remove the last item from a array.
+// numbers.shift() for remove the first element from a array 
+// numbers.unshift(22); for add a item on a array as a first element 
+console.log(numbers);
