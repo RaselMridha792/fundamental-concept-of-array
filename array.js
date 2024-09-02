1f8ba61a4d769489a1ff72ef9e9419b52bab1505
rasel mridha <raselmridha792@gmail.com>
2024-09-03 02:40:11 +0600

    basic functions of array

--- array.js
+++ array.js
@@ -5,4 +5,15 @@ const numbers = [12, 33, 44, 4];
 // numbers.pop() for remove the last item from a array.
 // numbers.shift() for remove the first element from a array 
 // numbers.unshift(22); for add a item on a array as a first element 
-console.log(numbers);
+
+// *********************** for knowing any items of a array 
+// console.log(numbers.includes(12));
+// console.log(numbers.includes(212));
+// if (numbers.includes(22)){
+//     console.log("yes 22 is here");
+// }
+// else{
+//     console.log("no 22 is not here");
+// }
+
+// **************** for knowing of a index of elements ************************ 
